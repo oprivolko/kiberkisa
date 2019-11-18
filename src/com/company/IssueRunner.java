b089f2c742846d11d9f166886bd0a6bf0579b68e
oprivolko <57415910+oprivolko@users.noreply.github.com>
2019-11-18 23:28:38 +0300

    1 commit

---
+++ src/com/company/IssueRunner.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class IssueRunner {
+    public static void main(String[] args) {
+        DigitCounter dc=new DigitCounter();
+ System.out.println(dc.calculate(5678));
+    }
+}
